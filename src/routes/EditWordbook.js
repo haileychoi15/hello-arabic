1d43aed0c3b16d4f42cc61691449cfad5506393a
yuyeong choi <imdud0612@gmail.com>
2020-10-19 03:57:07 +0900

    build router

---
+++ src/routes/EditWordbook.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function EditWordbook() {
+    return;
+}
+
+export default EditWordbook;
---
+++ src/routes/Wordbook.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Wordbook() {
+    return;
+}
+
+export default Wordbook;
