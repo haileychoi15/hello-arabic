27e36886c18af9ce87485f3036f2b15e2c68a498
yuyeong choi <imdud0612@gmail.com>
2020-10-22 01:12:23 +0900

    add a word to firebase

--- src/myFirebase.js
+++ src/myFirebase.js
@@ -1,5 +1,6 @@
 import * as firebase from "firebase/app";
 import 'firebase/auth';
+import 'firebase/firestore';
 
 var firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
@@ -14,3 +15,4 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 export const firebaseInstance = firebase;
 export const authService = firebase.auth();
+export const dbService = firebase.firestore();
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,65 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {dbService} from "../myFirebase";
 
 function Home() {
-    return <div>Home</div>;
+    const [value, setValue] = useState('');
+    const [lang, setLang] = useState(true); // arabic
+    const [added, setAdded] = useState(false);
+    const [list, setList] = useState([]);
+
+    const getWords = async () => {
+        const words = await dbService.collection("words").get();
+        words.forEach(document => {
+            const wordObject = {
+                ...document.data(),
+                id: document.id
+            }
+            setList(prev => [wordObject, ...prev]);
+        });
+    }
+
+    useEffect(() => {
+        getWords();
+    },[]);
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        const data = await dbService.collection('words').add({
+            word: value,
+            date: Date.now()
+        });
+        if(data) setAdded(true);
+    }
+    const onChange = (e) => {
+        const { value } = e.target;
+        setValue(value);
+        setAdded(false);
+    }
+    const onToggleLang = () => {
+        setLang(lang => !lang);
+    }
+    const onToggleSearch = () => {
+    }
+
+    console.log(list);
+    return (
+        <div>
+            <form action="" onSubmit={onSubmit}>
+                <input type="text" placeholder="단어를 입력하세요." lang="ar" maxLength={30} value={value} onChange={onChange} />
+                {added && <span>단어장에 추가되었습니다.</span>}
+                <button type="button" onClick={onToggleSearch}>단어찾기</button>
+                <button type="button" onClick={onToggleLang}>
+                    {lang ? '아랍어 -> 영어' : '영어 -> 아랍어'}
+                </button>
+                <input type="submit" value="단어장 추가" />
+            </form>
+            <div>
+                <ul>
+                    {list.map(word => <li key={word.id}>{word.word}</li>)}
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 export default Home;
