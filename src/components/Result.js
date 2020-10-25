6b88acbe15dd55654b51c65abf80cf29de20b14b
yuyeong choi <imdud0612@gmail.com>
2020-10-26 04:39:59 +0900

    get word results using API

---
+++ src/components/Result.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Result( { results }) {
+
+    const decodeHtml = (html) => {
+        const element = document.createElement('textarea');
+        element.innerHTML = html;
+        return element.value;
+    }
+
+    return (
+        <div>
+            {results.map((result, index) => (
+                <div key={index}>
+                    <span>{decodeHtml(result.solution.form)}</span>&nbsp;
+                    <span>{decodeHtml(result.solution.vocForm)}</span>&nbsp;
+                    <span>{decodeHtml(result.solution.root)}</span>&nbsp;
+                    <span>{result.solution.niceGloss}</span>&nbsp;
+                    <span>{result.solution.posNice}</span>&nbsp;
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default Result;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,15 +1,17 @@
 import React, {useContext, useState} from 'react';
 import {dbService} from "../myFirebase";
+import axios from 'axios';
 import {UserContext} from "../Context";
+import Result from "../components/Result";
 
 function Home({ collectionPath }) {
     const userObj = useContext(UserContext)[0];
     const [value, setValue] = useState('');
     const [lang, setLang] = useState(true); // arabic
     const [added, setAdded] = useState(false);
+    const [results, setResults] = useState(null);
 
-    const onSubmit = async (e) => {
-        e.preventDefault();
+    const onToggleAdd = async () => {
         const date = new Date();
         const data = await dbService.collection(collectionPath).add({
             creator: userObj.uid,
@@ -26,7 +28,12 @@ function Home({ collectionPath }) {
     const onToggleLang = () => {
         setLang(lang => !lang);
     }
-    const onToggleSearch = () => {
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        let proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+        let targetUrl = `${process.env.REACT_APP_WORD_RESULT_API}{"word": "زَهْرَة", "dictionary": "AR-EN-WORD-DICTIONARY"}&format=json`;
+        const { data : { result } } = await axios.get(proxyUrl + targetUrl);
+        setResults(result);
     }
     return (
         <div>
@@ -41,14 +48,17 @@ function Home({ collectionPath }) {
                 {added &&
                 <span>단어장에 추가되었습니다.</span>
                 }
-                <button type="button" onClick={onToggleSearch}>
+                <button type="submit">
                     단어찾기
                 </button>
                 <button type="button" onClick={onToggleLang}>
                     {lang ? '아랍어 -> 영어' : '영어 -> 아랍어'}
                 </button>
-                <input type="submit" value="단어장 추가" />
+                <input type="button" onClick={onToggleAdd} value="단어장 추가" />
             </form>
+            <div>
+               {results && <Result results={results} />}
+            </div>
         </div>
     );
 }
