d3d60db92c72704914ce9d6a85888eb9f8bc9c49
yuyeong choi <imdud0612@gmail.com>
2020-10-28 22:06:13 +0900

    create message state in Home component

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -10,6 +10,7 @@ function Home({ collectionPath }) {
     const [lang, setLang] = useState(true); // arabic
     const [added, setAdded] = useState(false);
     const [results, setResults] = useState(null);
+    const [message, setMessage] = useState('');
 
     const onToggleAdd = async () => {
         const date = new Date();
@@ -20,25 +21,35 @@ function Home({ collectionPath }) {
         });
         if(data) setAdded(true);
     }
+
     const onChange = (e) => {
         const { value } = e.target;
         setValue(value);
         setAdded(false);
     }
+
     const onToggleLang = () => {
         setLang(lang => !lang);
     }
+
     const onSubmit = async (e) => {
         e.preventDefault();
-        const result = await getWordResult("زَهْرَة");
-        setResults(result);
+        setResults(null);
+
+        const hasVal = value.length;
+        setMessage( hasVal ? '결과를 가져오는 중...' : '단어를 입력해주세요.');
+        if (!hasVal) return false;
+
+        const test = 'زَهْرَة';
+        const result = await getWordResult(value);
+        result.length ? setResults(result) : setMessage('검색결과가 없습니다.');
     }
     return (
         <div>
             <form action="" onSubmit={onSubmit}>
                 <input
                     type="text"
-                    placeholder="단어를 입력하세요."
+                    placeholder={lang ? '아랍어 => 영어' : '영어 => 아랍어'}
                     lang="ar"
                     maxLength={30}
                     value={value}
@@ -50,12 +61,16 @@ function Home({ collectionPath }) {
                     단어찾기
                 </button>
                 <button type="button" onClick={onToggleLang}>
-                    {lang ? '아랍어 -> 영어' : '영어 -> 아랍어'}
+                    {!lang ? '아랍어 => 영어' : '영어 => 아랍어'}
                 </button>
-                <input type="button" onClick={onToggleAdd} value="단어장 추가" />
+                {results &&
+                    <input type="button" onClick={onToggleAdd} value="단어장 추가" />
+                }
             </form>
             <div>
-               {results && <Result results={results} />}
+               {results ? <Result results={results} />
+                        : <span>{message}</span>
+               }
             </div>
         </div>
     );
